412abc106527704908fa6b4f92217d50bdc747ef
Ky Lucero <kileelucero@gmail.com>
2020-12-16 20:35:22 -0700

    Reworked

--- server.js
+++ server.js
@@ -19,19 +19,6 @@ app.set("view engine", "handlebars");
 
 app.use(routes);
 
-const con = require("./config/connection")
-
-app.get("/ooga", (req, res) =>{
-  connection.query(`CREATE TABLE burgers
-  (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (id)
-  );
-  `)
-})
-
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
 });
